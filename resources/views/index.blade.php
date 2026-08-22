9e7cf099df6a8b4bb526c2c7ded6434c6e385f16
Klaus Essig Schweizer <klaus@spmodelismo.com.br>
2026-08-22 19:23:11 -0300

    index tela

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,50 @@
-<p>Oi</p>
+@extends('layouts.main-layout')
+
+@section('breadcrumb')
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active">Home</li>
+    </ol>
+@endsection
+@section('content')
+    <div class="d-flex justify-content-center">
+        <h2>Dashboard</h2>
+    </div>
+<!--TODO: complementar o dashboard para n ficar vazio e tirar o texto referencia-->
+        <div class="d-flex flex-wrap justify-content-center align-items-center">
+            <a href="{{route('vehicles')}}" class="text-decoration-none">
+                <div class="card text-white bg-primary mb-3 mx-2" style="max-width: 20rem;">
+                    <div class="card-header"><i class="fas fa-car me-2" aria-hidden="true"></i>Veículos</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Cadastro de veículos</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="{{route('parking')}}" class="text-decoration-none">
+                <div class="card text-black bg-secondary mb-3 mx-2" style="max-width: 20rem;">
+                    <div class="card-header"><i class="fas fa-parking me-2" aria-hidden="true"></i>Estacionadas</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Cadastro de estacionadas</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="{{route('users')}}" class="text-decoration-none">
+                <div class="card text-black bg-info mb-3 mx-2" style="max-width: 20rem;">
+                    <div class="card-header"><i class="fas fa-user me-2" aria-hidden="true"></i>Usuários</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Cadastro de usuários</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </a>
+    </div>
+@endsection
